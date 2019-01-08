235895fff56cc051391daa9818fa9ec8eb4ca8ce
bdi-carlo <benjamin.97@live.fr>
2019-01-08 18:44:42 +0100

    Modif

--- src/app.js
+++ src/app.js
@@ -11,8 +11,29 @@ var imc = new Vue({
     calcul: function(){
       this.poids = document.getElementById("poids").value;
       this.taille = document.getElementById("taille").value;
-      this.res = this.poids/((this.taille/100)^2);
-      alert(`Votre IMC est de ${this.res}`);
+      this.res = (this.poids/((this.taille/100)^2)).toFixed(2);
+      if( this.poids.length == 0 || this.taille.length == 0 )
+        alert(`Veuillez indiquer correctement votre poids et votre taille!`);
+      else
+        alert(`Votre IMC est de ${this.res}`);
+    }
+  }
+})
+
+var dej = new Vue({
+  el: '#dej',
+
+  data:{
+    poids: 0,
+    taille: 0,
+
+
+    res: 0
+  },
+
+  methods:{
+    ajouteActivite: function( uneActivite ){
+
     }
   }
 })
