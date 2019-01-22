9b1ad95886231001a157f33c7f1e876357d42980
dmousmane <dmousmane@gmail.com>
2019-01-22 18:15:29 +0100

    ajout du boutton 'afficher details des dossiers

--- app/consulterdossier/views.py
+++ app/consulterdossier/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from calculdej.models import Categorie
+from calculdej.models import Activite
+from calculdej.models import Dossier
+from calculdej.models import Travail
+from django.contrib.auth.decorators import login_required
 
 @login_required
 def consulterdossier(request):
-    return render(request, 'calculdej/consulterdossier.html')
+
+    dossiers = Dossier.objects.filter(auteur=request.user)
+    return render(request, 'consulterdossier/consulterdossier.html', locals())
