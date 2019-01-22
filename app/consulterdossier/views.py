7a6d4259c607e34a08cce67c09dac845f84d1358
dmousmane <dmousmane@gmail.com>
2019-01-22 20:13:48 +0100

    ajout de la restriction pour l'acces a une page

--- app/consulterdossier/views.py
+++ app/consulterdossier/views.py
@@ -3,10 +3,20 @@ from calculdej.models import Categorie
 from calculdej.models import Activite
 from calculdej.models import Dossier
 from calculdej.models import Travail
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+# from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required, user_passes_test
+
+
+def check_user(user):
+    if user.is_authenticated:
+        username = user.get_username()
+
+    return username=='admin'
 
 @login_required
+@user_passes_test(check_user)
 def consulterdossier(request):
-
     dossiers = Dossier.objects.filter(auteur=request.user)
     return render(request, 'consulterdossier/consulterdossier.html', locals())
