29a8c634a47d312b5c83497890cf684f830f8ad5
bdi-carlo <benjamin.97@live.fr>
2019-01-27 23:09:54 +0100

    fix imc

--- app/calculimc/views.py
+++ app/calculimc/views.py
@@ -5,11 +5,11 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def calculimc(request):
+    imc = 0
+    
     formImc = CalculImcForm(request.POST or None)
     if formImc.is_valid():
         poids = formImc.cleaned_data['poids']
         taille = (formImc.cleaned_data['taille'])/100
         imc = round((poids/(taille*taille)),2)
-    else:
-        imc = 0
     return render(request, 'calculimc/calculimc.html', locals())
