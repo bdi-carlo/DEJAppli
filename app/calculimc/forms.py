f6c61195cf06b1f4d5e1975755ee69fd2ee17c41
bdi-carlo <benjamin.97@live.fr>
2019-03-19 15:14:31 +0100

    IMC fixed

--- app/calculimc/forms.py
+++ app/calculimc/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
 class CalculImcForm(forms.Form):
-    poids = forms.DecimalField(decimal_places=2,min_value=0.1)
-    taille = forms.DecimalField(decimal_places=2,min_value=0.1)
+    poids = forms.DecimalField(decimal_places=2,min_value=0.1, label="Poids (en kg)")
+    taille = forms.DecimalField(decimal_places=2,min_value=0.1, label="Taille (en cm)")
+    age = forms.BooleanField(initial=False, label="J'ai 17 ans ou plus", required=False)
